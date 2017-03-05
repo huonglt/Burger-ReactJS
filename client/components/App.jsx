3da0a4ecedfc93b07c9b47f81188d4642aee7a19
hle <hle@example.com>
2017-03-05 22:59:39 +1300

    add the footer

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from './NavBar.jsx';
+import Footer from './Footer.jsx';
 export default class App extends React.Component {
   render() {
     return (
@@ -8,6 +9,7 @@ export default class App extends React.Component {
         <div className="container">
           {this.props.children}
         </div>
+        <Footer/>
       </div>
     );
   }
---
+++ client/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <p>(Code 4 Fun) & (Fun 2 Code)</p>
+      </div>
+    </div>
+  );
+}
