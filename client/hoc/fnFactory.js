34f03b131deb8abec9aa57d91029f910835c2d98
hle <hle@example.com>
2017-03-06 21:53:06 +1300

    create function findStepIndex. Refactor factories nextFnFactory and backFnFactory

--- client/hoc/fnFactory.js
+++ client/hoc/fnFactory.js
@@ -1,11 +1,16 @@
 import { browserHistory } from 'react-router';
 export const STEPS = ['Bun', 'Meat', 'Cheese', 'Salad', 'Extra'];
 const ROOT = '/Burger/';
+
+/*
+ * Find the index of the currentStep in the STEPS
+ */
+const findStepIndex = (stepName) => STEPS.findIndex(step => step == stepName);
 /*
  * A factory for the next function using in the OrderButtonBar
  */
-export const nextFnFactory = (currentStep) => {
-  let index = STEPS.findIndex((step) => step == currentStep);
+export const nextFnFactory = (stepName) => {
+  let index = findStepIndex(stepName);
   let routeIndex = 0;
   if(index >= 0 && index < STEPS.length - 1) {
     routeIndex = index + 1;
@@ -18,8 +23,8 @@ export const nextFnFactory = (currentStep) => {
 /*
  * A factory for the back function using in the OrderButtonBar
  */
-export const backFnFactory = (currentStep) => {
-  let index = STEPS.findIndex((step) => step == currentStep);
+export const backFnFactory = (stepName) => {
+  let index = findStepIndex(stepName);
   let routeIndex = 0;
   if(index >= 1) {
     routeIndex = index - 1;
