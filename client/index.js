a0ea22872fbeace6fb8132f62348a9e4d2bb2258
Le <helen.le@fnzc.co.nz>
2017-03-07 11:50:31 +1300

    enable redux dev tools for debugging

--- client/index.js
+++ client/index.js
@@ -9,7 +9,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './styles/main.css';
 
-let store = createStore(reducers);
+let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
