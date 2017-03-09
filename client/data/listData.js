2a19e733c9d4b6c9ae422af605976d1623936d90
Le <helen.le@fnzc.co.nz>
2017-03-10 10:05:52 +1300

    clean up indentation

--- client/data/listData.js
+++ client/data/listData.js
@@ -1,16 +1,32 @@
-export const BUNS = [{name: 'Sesame Bun', topImg: 'bun1-top.png', bottomImg: 'bun1-bottom.png'},
-                      {name: 'Glazed Bun', topImg: 'bun2-top.png', bottomImg: 'bun2-bottom.png'},
-                      {name: 'Hot Crossed Bun', topImg: 'bun3-top.png', bottomImg: 'bun3-bottom.png'}];
+export const BUNS = [
+  { name: 'Sesame Bun', topImg: 'bun1-top.png', bottomImg: 'bun1-bottom.png' },
+  { name: 'Glazed Bun', topImg: 'bun2-top.png', bottomImg: 'bun2-bottom.png' },
+  { name: 'Hot Crossed Bun', topImg: 'bun3-top.png', bottomImg: 'bun3-bottom.png' }
+];
 
-export const MEATS = [{name: 'Chicken', img: 'meat2-chicken.png'}, {name: 'Beef', img: 'meat1-beef.png'}];
+export const MEATS = [
+  { name: 'Chicken', img: 'meat2-chicken.png' },
+  { name: 'Beef', img: 'meat1-beef.png' }
+];
 
-export const CHEESES = [{name: 'Cheddar', img: 'cheese1-cheddar.png'}, {name: 'Old English', img: 'cheese2-oldenglish.png'}];
+export const CHEESES = [
+  { name: 'Cheddar', img: 'cheese1-cheddar.png' },
+  { name: 'Old English', img: 'cheese2-oldenglish.png' }
+];
 
-export const SALADS = [{name: 'Pineapple', img: 'veg1-pineapple.png'}, {name: 'Onion', img: 'veg2-onions.png'},
-                       {name: 'Gerkhins', img: 'veg3-gerkhins.png'}, {name: 'Lettuce 1', img: 'veg4-lettuce1.png'},
-                       {name: 'Lettuce 2', img: 'veg5-lettuce2.png'},{name: 'Cucumber', img: 'veg6-cucumber.png'},
-                       {name: 'Tomato', img: 'veg7-tomato.png'}];
+export const SALADS = [
+  { name: 'Pineapple', img: 'veg1-pineapple.png' },
+  { name: 'Onion', img: 'veg2-onions.png' },
+  { name: 'Gerkhins', img: 'veg3-gerkhins.png' },
+  { name: 'Lettuce 1', img: 'veg4-lettuce1.png' },
+  { name: 'Lettuce 2', img: 'veg5-lettuce2.png' },
+  { name: 'Cucumber', img: 'veg6-cucumber.png' },
+  { name: 'Tomato', img: 'veg7-tomato.png' }
+];
 
-export const EXTRAS = [{name: 'Bacon', img: 'extra1-bacon.png'}, {name: 'Egg', img: 'extra2-egg.png'}];
+export const EXTRAS = [
+  { name: 'Bacon', img: 'extra1-bacon.png' },
+  { name: 'Egg', img: 'extra2-egg.png' }
+];
 
-export const INGREDIENTS = ['Bun', 'Meat', 'Cheese', 'Salad', 'Extra'];
+export const INGREDIENTS = [ 'Bun', 'Meat', 'Cheese', 'Salad', 'Extra' ];
