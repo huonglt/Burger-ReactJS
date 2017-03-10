2a8e20adf7cb3f6beb7278b8a3099437be240c46
Le Huong <lehuong@Le-Huongs-MBP.station>
2017-03-11 10:24:02 +1300

    append the relative path ../images to the image src

--- client/components/Image.jsx
+++ client/components/Image.jsx
@@ -1,2 +1,2 @@
 import React from 'react';
-export default (props) => <img src={props.src} className="img-responsive"/>
+export default (props) => <img src={'../images/' + props.src} className="img-responsive"/>;
