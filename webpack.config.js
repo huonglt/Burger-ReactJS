f3c573fafe5be96814a21e1bf12fe1e3f858c301
huonglt <lthuongvn@yahoo.com>
2017-03-28 19:20:25 +1300

    relocate the existing configuration to config/base.js. create a function to load the  webpack config file based on env variable in yarn script

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,4 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-	template: './client/index.html',
-	filename: 'index.html',
-	inject: 'body'
-});
-module.exports = {
-	entry: './client/index.js',
-	output: {
-		path: path.resolve('dist'),
-		filename: 'index_bundle.js',
-		publicPath: '/'
-	},
-	module: {
-		loaders: [
-			{ test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/},
-			{ test: /\.css$/, use: ['style-loader', 'css-loader']},
-			{ test: /\.(jpg|png|svg)$/, use: 'file-loader?name=images/[name].[ext]'}
-		]
-	},
-	plugins: [HtmlWebpackPluginConfig],
-	devServer: {
-		historyApiFallback: true
-	}
-}
+
+const buildConfig = (env) => require('./config/' + env + '.js');
+
+module.exports = buildConfig;
