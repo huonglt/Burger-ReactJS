fcb89b9cdf44b768d4b436a8567c8b74bbe34b73
huonglt <lthuongvn@yahoo.com>
2017-04-02 17:33:58 +1200

    create a separate chunk for react and other libs that are used more than 2 throughout the app.

--- webpack/prod.js
+++ webpack/prod.js
@@ -4,37 +4,38 @@ const merge = require('webpack-merge');
 const baseConfig = require('./base.js');
 
 module.exports = merge(baseConfig, {
-    devServer: {
-      port: 9090,
-      historyApiFallback: true
-    },
-    output: {
-  		path: path.resolve('dist'),
-      filename: '[name].[chunkhash].js'
-  	},
-  	plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        minimize: true,
-        sourceMap: true
-      }),
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('production')
-      }),
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false,
-        devtool: 'source-map'
-      }),
-      new webpack.optimize.CommonsChunkPlugin({
-  			name: 'vendor',
-        minChunks: function (module) {
-          return (module.context && module.context.indexOf('node_modules') !== -1);
-        }
-  		}),
-      new webpack.optimize.CommonsChunkPlugin({
-          name: 'manifest',
-          minChunks: Infinity
-      })
-    ]
+  entry: {
+    app: './client/index.js',
+    vendor: ['react']
+  },
+  devServer: {
+    port: 9090,
+    historyApiFallback: true
+  },
+  output: {
+		path: path.resolve('dist'),
+    filename: '[name].[chunkhash].js'
+	},
+	plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+      devtool: 'source-map'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+      minChunks: 2
+		}),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'manifest',
+        minChunks: Infinity
+    })]
   }
 );
